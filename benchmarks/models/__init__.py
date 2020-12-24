730a25ac799f0669e907d813df454cacfda67ddb
Maud LEMERCIER <ml3116@ic.ac.uk>
2020-12-24 15:13:23 +0000

    add benchmarks files

--- benchmarks/models/__init__.py
+++ benchmarks/models/__init__.py
@@ -1,4 +1,5 @@
 from .train_gpkconv1d import train_gpkconv1d_classifier
 from .train_gprnn import train_gprnn_classifier
 from .train_gpsig import train_gpsig_classifier
+from .train_gpsig_vosf import train_gpsig_vosf_classifier
 from .train_gpsigrnn import train_gpsigrnn_classifier
