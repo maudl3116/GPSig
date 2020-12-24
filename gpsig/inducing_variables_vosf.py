2cef70b09a6118c53ea8039ef5fb789b82252f2d
Maud LEMERCIER <ml3116@ic.ac.uk>
2020-12-24 14:33:15 +0000

    rename last added files so that the folder is better organised

---
+++ gpsig/inducing_variables_vosf.py
@@ -0,0 +1,204 @@
+import numpy as np
+import tensorflow as tf
+import gpflow
+
+from gpflow import settings, transforms
+from gpflow.features import InducingFeature, Kuu, Kuf
+from gpflow.dispatch import dispatch
+from gpflow.decors import params_as_tensors, params_as_tensors_for, autoflow
+from gpflow.params import Parameter, ParamList
+from gpflow.kernels import Kernel, Combination, Sum, Product
+from .kernels import SignatureKernel
+
+from .kernels_pde import UntruncSignatureKernel 
+from .utils import get_powers, compute_trunc
+# from iisignature_tensorflow import Sig
+# import iisignature
+
+from tensorflow.python.framework import ops
+
+class SignatureOrthogonalInducing(InducingFeature):
+    """
+    Base class for VOSF inducing variables for use with signature kernel in sparse variational GPs.
+    """
+    def __init__(self):
+        super().__init__()
+
+
+class UntruncInducingOrthogonalTensors(SignatureOrthogonalInducing):
+    """
+    # Inducing class for using the vosf inducing variables with the PDE signature kernel.
+    # # Input
+    # :input_dim:     the total size of an input sample to the kernel
+    # :d:             the state-space dimension of the data streams
+    # :M:             the number of inducing variables
+    """
+
+    def __init__(self, input_dim, d, M,**kwargs):
+       
+        super().__init__(**kwargs)
+        self.input_dim = input_dim
+        self.d = d
+        self.M = M
+
+        # compute the truncation level the "closest to M"
+        self.sig_level = compute_trunc(M,d)
+
+    def __len__(self):
+        return self.M
+
+@dispatch(UntruncInducingOrthogonalTensors, UntruncSignatureKernel, object)
+def Kuu_Kuf_Kff(feat, kern, X_new, *, jitter=0.0, full_f_cov=False, fast=False):
+    ''' 
+    In PDE-VOSF: 
+        Kxx is computed with the PDE-signature kernel. 
+        Kzz is the identity
+        Kzx is a batch of signatures.
+    '''
+
+    with params_as_tensors_for(feat,kern):
+
+        # Computing Kzz (the identity!)
+        Kzz = Kuu(feat,kern) 
+
+        # Computing Kzx 
+        if fast: # this method is in development. It avoids the precomputation of signatures.
+            Kzx = None 
+        else:
+            Kzx = Kuf(feat,kern,X_new[:,feat.input_dim:]) # S(\theta x) using iisignature compatible with tensorflow even if we do not use its autodiff
+        
+        # Computing Kxx (with the PDE-signature kernel here)
+        Kxx = kern.Kdiag(X_new[:,:feat.input_dim]) 
+        if full_f_cov:
+            Kxx += jitter * tf.eye(tf.shape(X)[0], dtype=settings.dtypes.float_type)
+        else:
+            Kxx += jitter
+
+    return Kzz, Kzx, Kxx
+
+@dispatch(UntruncInducingOrthogonalTensors, UntruncSignatureKernel, object)
+def Kuf(feat, kern, X_new):
+    ''' (Kuf)_{m,i}=S^m(x_i) 
+    There are three options: 
+    (1) the signatures are precomputed and concatenated with the input streams. 
+    (2) we can use a trick to avoid computing any signatures (this constrains the form of the variational parameters)
+    (3) we compute the signatures with iisignature (this option is now commented)
+    ''' 
+
+    with params_as_tensors_for(feat,kern):
+        
+        num_examples = tf.shape(X_new)[0]
+
+        # X, _ = kern._slice(X_new, None)
+        # X = tf.reshape(X, (num_examples, -1, feat.d))
+        # S_tf = Sig(X,feat.sig_level)  # (N,M)
+        
+        S_tf = X_new 
+        
+        indices = get_powers(feat.d,feat.sig_level)[:(feat.M-1),:] # this way we do not need to differentiate the signature wrt its input
+        levels = tf.repeat(kern.lengthscales[None,:],repeats=tf.shape(indices)[0],axis=0)
+        powers_levels = tf.pow(levels,indices)
+        powers = tf.math.reduce_prod(powers_levels,axis=1)
+        S_tf/=powers[None,:]
+        ones = tf.ones([num_examples,1],dtype=settings.dtypes.float_type)
+        full_S = tf.concat([ones,S_tf],axis=1)
+        full_S *= tf.sqrt(kern.sigma)
+        Kzx = tf.transpose(full_S)
+
+    return Kzx
+
+@dispatch(UntruncInducingOrthogonalTensors, UntruncSignatureKernel)
+def Kuu(feat, kern):
+    ''' Kuu = (<S^i(.),S^j(.)>)_i,j is diagonal since the signature features are orthogonal ''' 
+    ''' The diagonal elements depend on the kernel hyperparameters kern.sigma and kern.lengthscale'''
+    with params_as_tensors_for(feat,kern):
+        Kzz = tf.eye(feat.M, dtype=settings.dtypes.float_type) 
+
+    return Kzz
+
+class TruncInducingOrthogonalTensors(SignatureOrthogonalInducing):
+    """
+    # Inducing class for using the vosf inducing variables with the truncated signature kernel.
+    # # Input
+    # :input_dim:     the total size of an input sample to the kernel
+    # :d:             the state-space dimension of the data streams
+    # :M:             the number of inducing variables
+    """
+
+    def __init__(self, input_dim, d, M,**kwargs):
+       
+        super().__init__(**kwargs)
+        self.input_dim = input_dim
+        self.d = d
+        self.M = M
+
+        # compute the truncation level the "closest to M"
+        self.sig_level = compute_trunc(M,d)
+
+    def __len__(self):
+        return self.M
+
+@dispatch(TruncInducingOrthogonalTensors, SignatureKernel, object)
+def Kuu_Kuf_Kff(feat, kern, X_new, *, jitter=0.0, full_f_cov=False, fast=False):
+    ''' 
+    In Trunc-VOSF: 
+        Kxx is computed with the truncated signature kernel. 
+        Kzz is the identity
+        Kzx is a batch of signatures, which may be precomputed.
+    '''
+
+    with params_as_tensors_for(feat,kern):
+
+        # Computing Kzz (the identity!)
+        Kzz = Kuu(feat,kern)
+
+        # Computing Kzx
+        if fast: # in development
+            Kzx = None
+        else:
+            Kzx = Kuf(feat,kern,X_new[:,feat.input_dim:]) 
+
+        # Computing Kxx (with the truncated signature kernel here)
+        Kxx = kern.K(X_new[:,:feat.input_dim],return_levels = False)  
+        if full_f_cov:
+            Kxx += jitter * tf.eye(tf.shape(X)[0], dtype=settings.dtypes.float_type)
+        else:
+            Kxx += jitter
+    return Kzz, Kzx, Kxx
+
+@dispatch(TruncInducingOrthogonalTensors, SignatureKernel, object)
+def Kuf(feat, kern, X_new):
+    ''' (Kuf)_{m,i}=S^m(x_i) 
+    There are three options: 
+    (1) the signatures are precomputed and concatenated with the input streams. 
+    (2) We can use a trick to avoid computing any signatures (this constrains the form of the variational parameters)
+    (3) we compute the signatures with iisignature (this option is now commented)
+    ''' 
+    with params_as_tensors_for(feat,kern):
+        num_examples = tf.shape(X_new)[0]
+
+        # X, _ = kern._slice(X_new, None)
+        # X = tf.reshape(X, (num_examples, -1, feat.d))
+        # S_tf = Sig(X,feat.sig_level)  # (N,M)
+        
+        S_tf = X_new
+        
+        indices = get_powers(feat.d,feat.sig_level)[:(feat.M-1),:]
+        levels = tf.repeat(kern.lengthscales[None,:],repeats=tf.shape(indices)[0],axis=0)
+        powers_levels = tf.pow(levels,indices)
+        powers = tf.math.reduce_prod(powers_levels,axis=1)
+        S_tf/=powers[None,:]
+        ones = tf.ones([num_examples,1],dtype=settings.dtypes.float_type)
+        full_S = tf.concat([ones,S_tf],axis=1)
+        full_S *= tf.sqrt(kern.sigma)
+        Kzx = tf.transpose(full_S)
+
+    return Kzx
+
+@dispatch(TruncInducingOrthogonalTensors, SignatureKernel)
+def Kuu(feat, kern):
+    ''' Kuu = (<S^i(.),S^j(.)>)_i,j is diagonal since the signature features are orthogonal ''' 
+    ''' The diagonal elements depend on the kernel hyperparameters kern.sigma and kern.lengthscale'''
+    with params_as_tensors_for(feat,kern):
+        Kzz = tf.eye(feat.M, dtype=settings.dtypes.float_type) 
+    return Kzz
---
+++ gpsig/kernels_pde.py
@@ -0,0 +1,225 @@
+import tensorflow as tf
+import numpy as np
+
+from gpflow.params import Parameter
+from gpflow.decors import params_as_tensors, params_as_tensors_for, autoflow
+from gpflow import transforms
+from gpflow import settings
+from gpflow.kernels import Kernel
+
+from .sigKer_fast import sig_kern_diag as sig_kern_diag 
+from . import lags
+
+from tensorflow.python.framework import ops
+
+class UntruncSignatureKernel(Kernel):
+    """
+    """
+    def __init__(self, input_dim, num_features, lengthscales=1,
+                 order=0, num_lags=None, implementation = 'cython',name=None):
+        """
+        # Inputs:
+        ## Args:
+        :input_dim:         the total size of an input sample to the kernel
+        :num_features:      the state-space dimension of the input sequebces,
+
+        ## Kwargs:
+        ### Kernel options     
+		:lengthscales:      lengthscales for scaling the coordinates of the input sequences,
+                            if lengthscales is None, no scaling is applied to the paths
+                            if ARD is True, there is one lengthscale for each path dimension, i.e. lengthscales is of size (num_features)
+        :order              corresponds to the level of discretization to solve the PDE to approximate the signature kernel
+        :num_lags:          Nonnegative integer or None, the number of lags added to each sequence. Usually between 0-5.
+        :implementation:    should be 'cython'. In the development of VOSF, we may as well test dedicated tensorflow operators 'gpu_op' or 'cpu_op'. 
+                            We may as well compare to a direct tensorflow implementation (with automatic differentiation) 'tf'.
+                            The 'cython' option computes the kernel (cython code) and its gradients (tf code).
+        TO DO:
+            allow num_lags to be different from None
+        
+        """
+        
+        super().__init__(input_dim, active_dims, name=name)
+        self.num_features = num_features
+        self.len_examples = self._validate_number_of_features(input_dim, num_features)
+
+        assert num_lags is None, "VOSF does not handle lags yet"
+        assert implementation in ['cython','cpu_op', 'gpu_op','tf'], "implementation should be 'cython', 'cpu_op', 'gpu_op' or 'tf'.
+        self.implementation = implementation
+        self.order = order
+        self.sigma = Parameter(1., transform=transforms.positive, dtype=settings.float_type)
+        
+        if num_lags is None:
+            self.num_lags = 0	
+        else:
+            if not isinstance(num_lags, int) or num_lags < 0:
+                raise ValueError('The variable num_lags most be a nonnegative integer or None.')
+            else:
+                self.num_lags = int(num_lags)
+                if num_lags > 0:
+                    self.lags = Parameter(0.1 * np.asarray(range(1, num_lags+1)), transform=transforms.Logistic(), dtype=settings.float_type)
+                    gamma = 1. / np.asarray(range(1, self.num_lags+2))
+                    gamma /= np.sum(gamma)                   
+                    self.gamma = Parameter(gamma, transform=transforms.positive, dtype=settings.float_type)
+
+        if lengthscales is not None:
+            lengthscales = self._validate_signature_param("lengthscales", lengthscales, self.num_features)
+            self.lengthscales = Parameter(lengthscales, transform=transforms.positive, dtype=settings.float_type)
+        else:
+            self.lengthscales = None
+
+	######################
+	## Input validators ##
+	######################
+	
+    def _validate_number_of_features(self, input_dim, num_features):
+        """
+        Validates the format of the input samples.
+        """
+        if input_dim % num_features == 0:
+            len_examples = int(input_dim / num_features)
+        else:
+            raise ValueError("The arguments num_features and input_dim are not consistent.")
+        return len_examples
+
+    def _validate_signature_param(self, name, value, length):
+        """
+        Validates signature params
+        """
+        value = value * np.ones(length, dtype=settings.float_type)
+        correct_shape = () if length==1 else (length,)
+        if np.asarray(value).squeeze().shape != correct_shape:
+            raise ValueError("shape of parameter {} is not what is expected ({})".format(name, length))
+        return value
+
+    ########################################
+    ## Autoflow functions for interfacing ##
+    ########################################
+
+
+    @autoflow((settings.float_type, [None, None]),
+              (settings.float_type, [None, None]))
+    def compute_K(self, X, Y):
+        return self.K(X, Y)
+
+    @autoflow((settings.float_type, [None, None]))
+    def compute_K_symm(self, X):
+        return self.K(X)
+
+    @params_as_tensors
+    def _apply_scaling_and_lags_to_sequences(self, X):
+        """
+        Applies scaling and lags to sequences.
+        """
+        
+        num_examples, len_examples, _ = tf.unstack(tf.shape(X))
+        
+        num_features = self.num_features * (self.num_lags + 1)
+        
+        if self.num_lags > 0:
+            X = lags.add_lags_to_sequences(X, self.lags)
+
+        X = tf.reshape(X, (num_examples, len_examples, self.num_lags+1, self.num_features))
+        
+        if self.lengthscales is not None:
+            X /= self.lengthscales[None, None, None, :]
+
+        if self.num_lags > 0:
+            X *= self.gamma[None, None, :, None]
+        
+        X = tf.reshape(X, (num_examples, len_examples, num_features))
+        return X
+
+
+
+    @params_as_tensors
+    def Kdiag(self, X, presliced=False,name=None):
+        """
+        Computes the diagonal of a square signature kernel matrix. To be re-implemented, with custom autograd
+        """
+
+        num_examples = tf.shape(X)[0]
+            
+        if not presliced:
+            X, _ = self._slice(X, None)
+
+        X = tf.reshape(X, (num_examples, -1, self.num_features))
+        X = self._apply_scaling_and_lags_to_sequences(X)       
+
+        if self.implementation == 'cython':
+            K_diag = Kdiag_python(X,self.order)
+        # elif self.implementation == 'cpu_op':
+        #     K_diag, K_diag_rev = cov_module_cpu.untrunc_cov(X)
+        # elif self.implementation == 'gpu_op':
+        #     K_diag, K_diag_rev = cov_module_gpu.untrunc_cov(X)
+        # else:
+        #     K_diag = Kdiag_tf(X,n=5)
+        return self.sigma*K_diag[:,-1,-1]
+
+''' Functions for the Cython covariance operator ''' 
+
+def Kdiag_python(X,order=0,name=None):
+    with ops.name_scope(name, "Kdiag_python", [X]) as name:
+        K_diag, grad = py_func(sig_kern_diag,
+                                 [X,order],
+                                 [tf.float64, tf.float64],
+                                 name = name,
+                                 grad = _KdiagGrad
+        )
+        return K_diag
+
+
+def py_func(func,inp,Tout, stateful=True, name=None, grad=None):
+
+    rnd_name = 'PyFuncGrad' + str(np.random.randint(0,1E+8))
+
+    tf.RegisterGradient(rnd_name)(grad)
+    g = tf.get_default_graph()
+
+    with g.gradient_override_map({"PyFunc":rnd_name}):
+        return tf.py_func(func,inp,Tout,stateful=stateful, name=name)
+
+def _KdiagGrad(op, grad_next_op_Kdiag,grad_next_op_Kdiag_grad):
+    
+    ''' inputs:
+        - op: is pyfunc
+        - grad_next_op_Kdiag: <-> grad_{K_diag} K_diag[:,-1,-1]
+        - we do not use grad_next_op_Kdiag_grad
+    '''
+
+    n = op.inputs[1]
+    c = tf.math.pow(2,n)
+    cfloat = tf.dtypes.cast(c,tf.float64)
+
+    X = op.inputs[0]
+    K_diag = op.outputs[0]
+    K_diag_rev = op.outputs[1]
+    
+    # get shapes back
+    A = tf.shape(X)[0]
+    M = tf.shape(X)[1]
+    D = tf.shape(X)[2]
+  
+    # to compute the real gradient, as K_diag_rev is only one step of that computation
+    inc_X = (X[:,1:,:]-X[:,:-1,:])/cfloat  #(A,M-1,D)  increments defined by the data                
+    inc_X = tf.repeat(inc_X, repeats=2**n, axis=1) #(A,(2**n)*(M-1),D)  increments on the finer grid
+    
+    # Reorganize the K_rev matrix
+    K_rev_rev = tf.reverse(K_diag_rev,axis=[1],name='reverse1')
+    K_rev_rev = tf.reverse(K_rev_rev,axis=[2],name='reverse2')
+
+    KK = (K_diag[:,:-1,:-1] * K_rev_rev[:,1:,1:])                   # (A,(2**n)*(M-1),(2**n)*(N-1))
+
+    K_grad = KK[:,:,:,None]*inc_X[:,None,:,:]                       # (A,(2**n)*(M-1),(2**n)*(N-1),D)
+    
+    n = tf.dtypes.cast(n, tf.float64)
+
+    K_grad = (1./cfloat)*tf.reduce_sum(K_grad,axis=2)               # (A,(2**n)*(M-1),D)
+
+    K_grad =  tf.reduce_sum(tf.reshape(K_grad,(A,M-1,c,D)),axis=2)  # (A,M-1,D)
+
+    # The gradient { grad_{X_i}[K(X_i,X_i)] : shape M,D } -> shape A,M,D 
+    # we need to multiply by 2, because we have only computed the left gradient of the bilinear function
+    grad_points = -2.*tf.concat([K_grad,tf.zeros((A, 1, D),dtype=tf.float64)],axis=1) + 2.*tf.concat([tf.zeros((A, 1, D),dtype=tf.float64), K_grad], axis=1)
+    
+    return grad_next_op_Kdiag[:,-1,-1][:,None,None]*grad_points, None
+
