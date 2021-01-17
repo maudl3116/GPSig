fix error normalization

gpsig/inducing_variables_vosf.py
@@ -182,7 +182,7 @@ def Kuu_Kuf_Kff(feat, kern, X_new, *, jitter=0.0, full_f_cov=False, fast=False):
                 # Kxx would be 1 for all x (or sum of variances if not set to 1), but here we need to get back the norms 
                 # to corrext Kzx. 
                 Kxx, Kxx_un = kern.K_norms(X_new[:,:feat.input_dim])
-                Kzx /= tf.repeat(Kxx_un,repeats=np.array([feat.d**i for i in range(feat.sig_level+1)]),axis=0)
+                Kzx /= tf.repeat(Kxx_un[:(feat.sig_level+1)],repeats=np.array([feat.d**i for i in range(feat.sig_level+1)]),axis=0)
                 Kzx *= tf.sqrt(kern.sigma)
             else:
                 Kxx = kern.Kdiag(X_new[:,:feat.input_dim],return_levels = False) 