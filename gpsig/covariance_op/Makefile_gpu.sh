948c48403107d23643b42f1e4e3bf69bd721efc4
maudl3116 <maud.lemercier@warwick.ac.uk>
2021-02-11 14:17:09 +0000

    update the makefile

--- gpsig/covariance_op/Makefile_gpu.sh
+++ gpsig/covariance_op/Makefile_gpu.sh
@@ -1,9 +1,10 @@
 #/bin/bash
-CUDA_ROOT=/usr/local/cuda-10.1
-TF_ROOT=/tensorflow-1.15.2/python3.6/tensorflow_core
+CUDA_ROOT=/usr/local/cuda/
+TF_ROOT=/anaconda/envs/sigGPDE_env/lib/python3.7/site-packages/tensorflow_core
+
 
 ln -s ${TF_ROOT}/libtensorflow_framework.so.1 ${TF_ROOT}/libtensorflow_framework.so
 
 ${CUDA_ROOT}/bin/nvcc -std=c++11 -c -o untrunc_cov_op_gpu.cu.o untrunc_cov_op_gpu.cu -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
-g++ -std=c++11 untrunc_cov_op_gpu.cc untrunc_cov_op_gpu.cu.o -o untrunc_cov_op_gpu.so -shared -fPIC -I ${TF_ROOT}/include -I ${CUDA_ROOT}/include -I ${TF_ROOT}/include/external/nsync/public -lcudart -L ${CUDA_ROOT}/lib64/ -L${TF_ROOT} -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 untrunc_cov_op_gpu.cc untrunc_cov_op_gpu.cu.o -o untrunc_cov_op_gpu.so -shared -fPIC -I ${TF_ROOT}/include -I ${CUDA_ROOT}/include -I ${TF_ROOT}/include/external/nsync/public -lcudart -L ${CUDA_ROOT}/lib64/ -L${TF_ROOT} -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=1
